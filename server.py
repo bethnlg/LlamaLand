2d90edb8b242ca28f8763461aaed28b574258364
Carlos Alvarez Ponce <carlin_ap81@hotmail.com>
2024-07-30 11:20:57 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 # Versiones disponibles
 # forge, mohist, fabric, vanilla, paper
 
-# Puedes instalar mohist después de instalar forge desde el menú de gestionar
-# Puedes instalar paper después de instalar vanilla desde el menú de gestionar
-# Puedes instalar purpur después de instalar fabric desde el menú de gestionar
+# Se puede instalar mohist después de instalar forge desde el menú de gestionar
+# Se puede instalar paper después de instalar vanilla desde el menú de gestionar
+# Se puede instalar purpur después de instalar fabric desde el menú de gestionar
 
 # Regiones de ngrok
 # Código          Lugar
@@ -30,7 +30,7 @@
 
 
 
-# No toques nada de aquí para abajo, puedes dañarlo
+
 import requests,os,base64,glob,time
 if os.path.exists("servidor.py"):
 	os.remove("servidor.py")
